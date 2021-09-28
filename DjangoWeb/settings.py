71142f8ec2c6ef5137658fbe9840bb830ee46c2b
Allen Kung <90850299+AllenKung0505@users.noreply.github.com>
2021-09-28 14:36:24 +0800

    adjust

--- DjangoWeb/settings.py
+++ DjangoWeb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1u1op3por4^rc^8(w*ziwe8-h(0twhv4f!_3$s#it_a&fwq5xj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
